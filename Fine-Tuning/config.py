de47fd76a2353faaf2f8fb3f55deb22c9a5605c2
SimosPrks <proikakis.simos@gmail.com>
2025-03-05 21:59:08 +0100

    Data and FineTuning

---
+++ Fine-Tuning/config.py
@@ -0,0 +1,18 @@
+# Step: Fine-Tuning 
+# Fine_Tuning
+# finetuning.py
+
+# Data used for Training 
+TRAIN_JSON_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/Dataset_TRAIN.json"
+DEV_JSON_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/Dataset_DEV.json"
+TRAIN_IN_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/PoisonPy-train.in"
+TRAIN_OUT_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/PoisonPy-train.out"
+DEV_IN_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/PoisonPy-dev.in"
+DEV_OUT_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/PoisonPy-dev.out"
+TEST_IN_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/PoisonPy-test.in"
+TEST_OUT_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/data/data_for_fine_tuning/PoisonPy-test.out"
+
+MODEL_PATH = "codellama/CodeLlama-7b-Instruct-hf"
+LORA_ADAPTER_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/Modified_model/lora_adapter"
+MODIFIED_MODEL_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/Modified_model/merged_model"
+RESULTS_PATH = "C:/Users/proik/Desktop/thesis-data-poisoning-attack/Modified_model/results"
